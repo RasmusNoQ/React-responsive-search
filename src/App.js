3ea95d18cff5bb2136d2132927778e3efa24625d
Daniel Lucas <danyyel770@gmail.com>
2022-07-26 11:30:49 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React,{useState,useEffect} from 'react'
+import axios from 'axios';
+import { MDBTable,MDBTableHead,MDBTableBody,MDBRow,MDBCol,MDBContainer } from 'mdb-react-ui-kit';
+import './App.css';
+
+function App() {
+  const [data,setData] = useState([])
+
+  useEffect(()=>{
+    loadUserData();
+
+  },[])
+
+  const loadUserData = async () =>{
+    return await axios
+    .get("http://localhost:5000/users")
+    .then((response) => setData(response))
+    .catch((err) => console.log(err)) 
+  }
+
+  console.log("data",data);
+  return (
+   <MDBContainer>
+    <div style={{marginTop:"100px"}}>
+      <h2>Search, Filter, Sort and Pagination using JSON Fake Rest API</h2>
+      <MDBRow>
+        <MDBCol size="12"></MDBCol>
+      </MDBRow>
+    </div>
+   </MDBContainer>
+  );
+}
+
+export default App;
